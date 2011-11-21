0fc72874fb3ffa10c9b6b3031c564a1e68e1b664
Stephan Diederich <stephan.diederich@googlemail.com>
2011-11-21 13:37:21 +0100

    pass settingsReader filterdelegate along

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -643,6 +643,8 @@ CGRect IASKCGRectSwap(CGRect rect);
         }
         self.currentIndexPath = indexPath;
 		targetViewController.file = specifier.file;
+    targetViewController.settingsReader = [[IASKSettingsReader alloc] initWithFile:specifier.file
+                                                                    filterDelegate:self.settingsReader.filterDelegate];
 		targetViewController.title = specifier.title;
         targetViewController.showCreditsFooter = NO;
         [[self navigationController] pushViewController:targetViewController animated:YES];
